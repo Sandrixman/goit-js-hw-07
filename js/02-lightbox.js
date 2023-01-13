f6b0363e96b6808adffc373ce6949dce05cfb3a5
Alexander <sandrix@ukr.net>
2023-01-13 20:16:06 +0200

    completed task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,2 +1,32 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", createGaleryItem(galleryItems));
+
+gallery.addEventListener("click", toggleModal);
+
+function createGaleryItem(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+        </a>`;
+        })
+        .join("");
+}
+
+function toggleModal(e) {
+    e.preventDefault();
+
+    const modal = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+
+}
